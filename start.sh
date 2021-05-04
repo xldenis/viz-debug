193976b937b9232e743f516bb838a54bf37b9322
nicocti <nicocti@users.noreply.github.com>
2021-05-04 14:18:26 +0200

    fix startup script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-echo '/dev/ttyACM0 9990; /dev/ttyACM1 9991; /dev/ttyACM2 9992; /dev/ttyACM3 9993; /dev/ttyACM4 9994' | xargs -d ';' cargo run server
+echo '/dev/ttyACM0 9990 /dev/ttyACM1 9991 /dev/ttyACM2 9992 /dev/ttyACM3 9993 /dev/ttyACM4 9994' | xargs -n2 cargo run server
